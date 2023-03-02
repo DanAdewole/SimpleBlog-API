c0b09214655e77d404c547f0a3fbd1eef304662e
DanAdewole <50157693+DanAdewole@users.noreply.github.com>
2023-03-02 19:27:29 +0100

    change information about the api

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
         # "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
